36430e2bc17758120132df77eea3ccfa07da9a11
Samat Tankeyev <s.tankeyev@gmail.com>
2025-06-06 21:45:40 +0500

    Initial Commit

---
+++ BudgetTracker/BudgetTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackerApp.swift
+//  BudgetTracker
+//
+//  Created by Самат Танкеев on 06.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
